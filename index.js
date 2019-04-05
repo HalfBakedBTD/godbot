915a933dfee692e8844625877e941eb5233b85e6
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2019-04-04 22:55:22 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,59 @@
+const botconfig = require("./botconfig.json");
+const Discord = require("discord.js");
+const fs = require("fs");
+const bot = new Discord.Client({disableEveryone: true});
+bot.commands = new Discord.Collection();
+
+fs.readdir("./commands/", (err, files) => {
+
+  if(err) console.log(err);
+
+  let jsfile = files.filter(f => f.split(".").pop() === "js")
+  if(jsfile.length <= 0){
+    console.log("Couldn't find commands.");
+    return;
+  }
+
+  jsfile.forEach((f, i) =>{
+    let props = require(`./commands/${f}`);
+    console.log(`${f} loaded!`);
+    bot.commands.set(props.help.name, props);
+  });
+
+});
+
+bot.on("ready", async () => {
+  console.log(`${bot.user.username} is online!`);
+
+  bot.user.setGame(`.help | ${bot.guilds.size} servers.`);
+});
+
+bot.on('guildCreate', guild => {
+  bot.user.setGame(`.help | ${bot.guilds.size} servers.`);
+});
+
+bot.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
+
+  let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
+  if(!prefixes[message.guild.id]){
+    prefixes[message.guild.id] = {
+      prefixes: "."
+    };
+  }
+  
+  let prefix = prefixes[message.guild.id].prefixes;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+  if (!message.content.startsWith(`${prefix}`)) {
+    return
+  }
+  
+  let commandfile = bot.commands.get(cmd.slice(prefix.length));
+  if(commandfile) commandfile.run(bot,message,args);
+  
+});
+
+bot.login(process.env.BOT_TOKEN);
