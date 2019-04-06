f7d8fc4417c35582f3888116eb17cc4284ce7f9a
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2019-04-05 22:59:45 -0400

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -8,21 +8,14 @@ module.exports.run = async (bot, message, args) => {
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("No can do pal! You don't have the perms!");
   let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
   if(!wUser) return message.reply("Couldn't find them!");
-  if(wUser.hasPermission("MANAGE_MESSAGES")) return message.reply("This user can't be warned because he/she has manage messages perms!");
   let reason = args.join(" ").slice(22);
-
-  warns[wUser.id].warns++;
-
-  fs.writeFile("./warnings.json", JSON.stringify(warns), (err) => {
-    if (err) console.log(err)
-  });
   
   wUser.send(`You have been warned in ${message.guild.name} for ${reason}`);
 
   let warnEmbed = new Discord.RichEmbed()
   .setTitle("⚠ WARNING ⚠")
   .setThumbnail("https://tse2.mm.bing.net/th?id=OIP.IJMhXskD06HXmBugmkU_tQHaEE&pid=Api")
-  .setDescription(`**Warned User:** <@${wUser.id}>\n**Warned By:** <@${message.author.id}>\n**Warned In:** ${message.channel}\n**Reason:** ${reason}\n**User Warn Level:** ${warns[wUser.id].warns}`)
+  .setDescription(`**Warned User:** <@${wUser.id}>\n**Warned By:** <@${message.author.id}>\n**Warned In:** ${message.channel}\n**Reason:** ${reason}`)
   .setColor("#f5f6fa")
 
   //let warnchannel = message.guild.channels.find(`name`, "zap-logs");
