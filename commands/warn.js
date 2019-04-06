ed3d85498ac4b05d1bf678e9e451a4a9f2d943fd
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2019-04-05 22:55:08 -0400

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -1,48 +1,39 @@
-const Discord = require("discord.js")
+const Discord = require("discord.js");
+const fs = require("fs");
+const ms = require("ms");
 
 module.exports.run = async (bot, message, args) => {
 
-let xdemb = new Discord.RichEmbed()
-        .setColor("RANDOM")
-        .setTitle("Ban Command")
-        .addField("Description:", `Ban a member`, true)
-        .addField("Usage:", `!ban [user] [reason]`, true)
-        .addField("Example:", `!ban @Bern spam`)
+  //!warn @daeshan <reason>
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("No can do pal! You don't have the perms!");
+  let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
+  if(!wUser) return message.reply("Couldn't find them!");
+  if(wUser.hasPermission("MANAGE_MESSAGES")) return message.reply("This user can't be warned because he/she has manage messages perms!");
+  let reason = args.join(" ").slice(22);
 
-        if(!message.member.hasPermission("BAN_MEMBERS") && message.author.id !== "505877805058359326") return message.channel.send("Sorry you don't have permission to use this!");
+  warns[wUser.id].warns++;
 
-        let member = message.mentions.members.first();
-        if(!member) return message.channel.send(xdemb)
-        if(!member.bannable) return message.channel.send("I can't ban this user!")
-        if(member.user.id === "505877805058359326") return message.channel.send("I can't ban my owner!")
+  fs.writeFile("./warnings.json", JSON.stringify(warns), (err) => {
+    if (err) console.log(err)
+  });
+  
+  wUser.send(`You have been warned in ${message.guild.name} for ${reason}`);
 
-        if(member.id === message.author.id) return message.channel.send("You can't ban your self")
+  let warnEmbed = new Discord.RichEmbed()
+  .setTitle("⚠ WARNING ⚠")
+  .setThumbnail("https://tse2.mm.bing.net/th?id=OIP.IJMhXskD06HXmBugmkU_tQHaEE&pid=Api")
+  .setDescription(`**Warned User:** <@${wUser.id}>\n**Warned By:** <@${message.author.id}>\n**Warned In:** ${message.channel}\n**Reason:** ${reason}\n**User Warn Level:** ${warns[wUser.id].warns}`)
+  .setColor("#f5f6fa")
 
-        let reason = args.slice(1).join(" ");
+  //let warnchannel = message.guild.channels.find(`name`, "zap-logs");
+  //if(!warnchannel) return message.reply("Couldn't find logs channel!");
 
-        if(!reason) {
-            res = "No reason given";
-        } else {
-            res = reason
-        }
-        await member.ban(reason).catch(error => message.channel.send(`Sorry, I coldn't ban because of: ${error}`));
+  //warnchannel.send(`⚠ WARNING ⚠\n\nWarned User: <@${wUser.id}>\n\nWarned By: <@${message.author.id}>\n\nWarned In: ${message.channel}\n\nWarns for ${wUser}: ${warns[wUser.id].warns}\n\nReason: ${reason}`);
 
-        let bean = new Discord.RichEmbed()
-        .setColor("RANDOM")
-        .setTitle(`Ban | ${member.user.tag}`)
-        .addField("User", member, true)
-        .addField("Moderator", message.author, true)
-        .addField("Reason", res)
-        .setTimestamp()
-
-        message.channel.send(bean)
-
-        message.delete()
+  warnchannel.send(warnEmbed)
 
 }
 
-
-
 module.exports.help = {
-  name: "ban"
+  name: "warn"
 }
