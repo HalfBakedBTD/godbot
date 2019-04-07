8bbc9a2848efffcefcdc7303884f307d020f3451
OmniFZ <49297330+OmniFZ@users.noreply.github.com>
2019-04-07 17:54:54 -0400

    Update infobot.js

--- commands/infobot.js
+++ commands/infobot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-	return message.channel.send("**Shinjan's Bot Invite Link:**\n\t :https://discordapp.com/api/oauth2/authorize?client_id=505877805058359326&permissions=8&scope=bott")
+	return message.channel.send("**Shinjan's Bot Invite Link:**\n\t :https://discordapp.com/api/oauth2/authorize?client_id=505877805058359326&permissions=8&scope=bott: **\n\t : Creator @NoChance#2019:")
 }
 
 module.exports.help = {
