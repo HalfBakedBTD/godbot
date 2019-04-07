455f4daeeb23be9f15941a877de2bdf5051311d5
OmniFZ <49297330+OmniFZ@users.noreply.github.com>
2019-04-07 13:01:17 -0400

    Update index.js

--- index.js
+++ index.js
@@ -39,17 +39,17 @@ bot.on("message", async message => {
   let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
   if(!prefixes[message.guild.id]){
     prefixes[message.guild.id] = {
-      prefixes: "."
+      prefixes: botconfig.prefix
     };
   }
-  
-  let prefix = prefixes[message.guild.id].prefixes;
+   let prefix = prefixes[message.guild.id].prefixes;
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
   if (!message.content.startsWith(`${prefix}`)) {
     return
   }
+
   
   let commandfile = bot.commands.get(cmd.slice(prefix.length));
   if(commandfile) commandfile.run(bot,message,args);
