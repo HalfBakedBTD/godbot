30612f727c6223ca8eae72e73cb7b7224766f347
OmniFZ <49297330+OmniFZ@users.noreply.github.com>
2019-04-07 12:29:20 -0400

    Delete info.js

--- commands/info.js
+++
@@ -1,9 +0,0 @@
-const Discord = require("discord.js");
-
-module.exports.run = async (bot, message, args) => {
-	return message.channel.send("**Shinjan Bot's Invite Link:**\n\t : link:https://discordapp.com/api/oauth2/authorize?client_id=505877805058359326&permissions=8&scope=bot")
-}
-
-module.exports.help = {
-  name: "info"
-}
