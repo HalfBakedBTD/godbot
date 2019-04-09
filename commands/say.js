fcae6122f4ff64153ce792572c1a24d738c0415f
OmniFZ <49297330+OmniFZ@users.noreply.github.com>
2019-04-08 21:50:29 -0400

    Create say.js

---
+++ commands/say.js
@@ -0,0 +1,15 @@
+
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+      if(!message.member.hasPermission("ADMINISTRATOR")) return;
+      const sayMessage = args.join(" ");
+      message.delete().catch();
+      message.channel.send(`🗨 I have decided to say:\n\`\`\`${sayMessage}\`\`\``);
+
+}
+
+module.exports.help = {
+  name: "say"
+}
