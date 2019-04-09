fb0768ed173b474f6b964a990a447495402d7015
OmniFZ <49297330+OmniFZ@users.noreply.github.com>
2019-04-08 22:20:59 -0400

    Update infobot.js

--- commands/infobot.js
+++ commands/infobot.js
@@ -1,69 +1,35 @@
 const Discord = require("discord.js");
 
-exports.run = async (client, message, args) => {
-    if (args[0]) {
-        let command = client.commands.has(args[0]) ? client.commands.get(args[0]) : (client.aliases.has(args[0]) ? client.aliases.get(args[0]) : null);
-        if (!command) return;
-        const embed = new Discord.RichEmbed()
-            .addField(`**${command.help.name.toProperCase()} Command**`, command.help.description)
-            .addField('Usage', command.help.usage)
-            .setColor("BLUE")
-        if (command.help.aliases) {
-            if (command.help.aliases.length > 0) embed.addField(`Aliases`, `\`${command.help.aliases.join(", ")}\``);
-        }
+module.exports.run = async (bot, message, args) => {
+    var text_channels = 0, voice_channels = 0;
+    bot.channels.array().forEach(channel => {
+      if (channel.type == 'text') {
+        text_channels += 1;
+      } else if (channel.type == 'voice') {
+        voice_channels += 1;
+      }
+    });
+    
+    let botAvatar = bot.user.displayAvatarURL;
+    let botEmbed = new Discord.RichEmbed()
+    .setColor("RANDOM")
+    .setThumbnail(botAvatar)
+    .addField("Name:", `${bot.user.username}`, true)
+    .addField("Guilds:", `${bot.guilds.size}`, true)
+    .addField("Users:", `${bot.users.size}`, true)
+    .addField("Text Channels:", `${text_channels}`, true)
+    .addField("Voice Channels:", `${voice_channels}`, true)
+    .addField("Created By:", `<@284137818895417344>`, true)
+    .addField("Language:", `discord.js`, true)
+    .addField("Memory:", `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed() + "MB"}`, true)
+    .addField("Birth:", `${bot.user.createdAt}`, true)
+    .addField("Commands:", "[Invite Link](https://discordapp.com/api/oauth2/authorize?client_id=505877805058359326&permissions=8&scope=bot) | [Commands](**Info**,**Kick**,**Ban**,**Say**,**Mute**,**Unmute**,**More Commands Added Daily**)");
+    
+    message.channel.send(botEmbed)
 
-        message.channel.send(embed);
-    } else {
-        if (client.config.useNewHelp == "true") {
-            var cats = new Map();
-
-            client.commands.forEach(c => {
-                if (c.help.enabled == true && c.help.hideHelp == false) {
-                    let n = c.help.type.toProperCase();
-                    if (!cats.has(n)) {
-                        cats.set(n, {
-                            name: n,
-                            array: []
-                        });
-                    };
-                    cats.get(n).array.push(`\`${c.help.name}\``);
-                }
-            });
-
-            var embed = new Discord.RichEmbed();
-            embed.setTitle(`Commands Info`);
-            embed.setDescription(`Use \`!help commandname\` to view help on a command. To see changelogs use \`!changlog\`. For additional help with a command, type \`!help <command name>\`.\nThank you for using Shinjan Bot! We know you will stick with us!`);
-            cats.forEach(cat => {
-                embed.addField(`**${cat.name}**`, cat.array.join(", "));
-            });
-            embed.setColor("RA DK ");
-            embed.setTimestamp();
-            embed.setThumbnail(client.user.displayAvatarURL);
-            embed.setFooter(`Created by Evens`);
-            message.channel.send(embed).catch((e) => {
-                message.channel.send(`Something went wrong! Tell a dev or try again.`);
-                console.error(e);
-            });
-        } else {
-            const embed = new Discord.RichEmbed()
-                .setColor("RANDOM")
-       .addField("**My Commands**"`mute` `kick` `ban` `purge` `say` `unmute` `info`")
-             
-            message.channel.send(embed)
-        }
-    }
-};
-
-exports.config = {
-aliases: [ ]
-};
-
-exports.help = {
-    enabled: true,
-    hideHelp: false,
-    type: "util",
-    name: "info",
-    aliases: ["halp", "commands", "cmds", "h"],
-    description: "The `help` command displays a help menu, which contains a list of commands and a way to find further help.",
-    usage: "`info"
-};
+    //message.reply(`here is my information:\n\n🤖 Name: ${bot.user.username}\n\n🕥 Created On: ${bot.user.createdAt}\n\n🌲 Online Users: ${bot.users.size}\n\n#⃣ Text Channels: ${text_channels}\n\n🔈 Voice Channels: ${voice_channels}\n\n🇯 Made With: discord.js\n\n🌙 Made By: HalfBakedGaming#6768 with ID: 346687165868015616\n\n🌐 Memory: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed() + "MB"}`);
+}
+  
+module.exports.help = {
+  name: "info"
+}
