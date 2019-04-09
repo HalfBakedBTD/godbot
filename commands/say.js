79e00a3d1c0a37d3e562df0371ec2dc19dcb647e
OmniFZ <49297330+OmniFZ@users.noreply.github.com>
2019-04-08 21:59:07 -0400

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
       if(!message.member.hasPermission("ADMINISTRATOR")) return;
       const sayMessage = args.join(" ");
       message.delete().catch();
-      message.channel.send(``\`\`${sayMessage}\`\`\``);
+      message.channel.send(`\`\`${sayMessage}\`\`\``);
 
 }
 
