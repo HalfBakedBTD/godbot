6460951435201417de808e2bec2cf58d80ad874c
OmniFZ <49297330+OmniFZ@users.noreply.github.com>
2019-04-08 22:13:57 -0400

    Update infobot.js

--- commands/infobot.js
+++ commands/infobot.js
@@ -1,10 +1,69 @@
 const Discord = require("discord.js");
 
-module.exports.run = async (bot, message, args) => {
-	.setColor("RANDOM")
-	return message.channel.send("**Shinjan's Bot Invite Link:**\n\t https://discordapp.com/api/oauth2/authorize?client_id=505877805058359326&permissions=8&scope=bot \n\t  Creator: 🔥NoChance🔥#2019\n\t```Information For Shinjan Bot``` \n\t**Help:**\n\t **Ban** .ban Omni \n\t **Kick** .kick omni\n\t **Uptime** .uptime \n\t**info** .info \n\t **Purge** .purge 200 \n\t **Mute** .mute Omni 1min Too Cool \n\t **Unmute** .unmute Omni Sry My Mistake, Your lit\n\t **Say** .say Omni Stop Being the best person in this universe!!")
-}
+exports.run = async (client, message, args) => {
+    if (args[0]) {
+        let command = client.commands.has(args[0]) ? client.commands.get(args[0]) : (client.aliases.has(args[0]) ? client.aliases.get(args[0]) : null);
+        if (!command) return;
+        const embed = new Discord.RichEmbed()
+            .addField(`**${command.help.name.toProperCase()} Command**`, command.help.description)
+            .addField('Usage', command.help.usage)
+            .setColor("BLUE")
+        if (command.help.aliases) {
+            if (command.help.aliases.length > 0) embed.addField(`Aliases`, `\`${command.help.aliases.join(", ")}\``);
+        }
 
-module.exports.help = {
-  name: "info"
-}
+        message.channel.send(embed);
+    } else {
+        if (client.config.useNewHelp == "true") {
+            var cats = new Map();
+
+            client.commands.forEach(c => {
+                if (c.help.enabled == true && c.help.hideHelp == false) {
+                    let n = c.help.type.toProperCase();
+                    if (!cats.has(n)) {
+                        cats.set(n, {
+                            name: n,
+                            array: []
+                        });
+                    };
+                    cats.get(n).array.push(`\`${c.help.name}\``);
+                }
+            });
+
+            var embed = new Discord.RichEmbed();
+            embed.setTitle(`Commands Info`);
+            embed.setDescription(`Use \`!help commandname\` to view help on a command. To see changelogs use \`!changlog\`. For additional help with a command, type \`!help <command name>\`.\nThank you for using Shinjan Bot! We know you will stick with us!`);
+            cats.forEach(cat => {
+                embed.addField(`**${cat.name}**`, cat.array.join(", "));
+            });
+            embed.setColor("RA DK ");
+            embed.setTimestamp();
+            embed.setThumbnail(client.user.displayAvatarURL);
+            embed.setFooter(`Created by Evens`);
+            message.channel.send(embed).catch((e) => {
+                message.channel.send(`Something went wrong! Tell a dev or try again.`);
+                console.error(e);
+            });
+        } else {
+            const embed = new Discord.RichEmbed()
+                .setColor("RANDOM")
+       .addField("**My Commands**"`mute` `kick` `ban` `purge` `say` `unmute` `info`")
+             
+            message.channel.send(embed)
+        }
+    }
+};
+
+exports.config = {
+aliases: [ ]
+};
+
+exports.help = {
+    enabled: true,
+    hideHelp: false,
+    type: "util",
+    name: "info",
+    aliases: ["halp", "commands", "cmds", "h"],
+    description: "The `help` command displays a help menu, which contains a list of commands and a way to find further help.",
+    usage: "`info"
+};
