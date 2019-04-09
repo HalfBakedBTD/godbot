57d8a6ebc006fcf84dbcaac3ddadf64bf89c62fc
OmniFZ <49297330+OmniFZ@users.noreply.github.com>
2019-04-08 21:52:22 -0400

    Update infobot.js

--- commands/infobot.js
+++ commands/infobot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-	return message.channel.send("**Shinjan's Bot Invite Link:**\n\t https://discordapp.com/api/oauth2/authorize?client_id=505877805058359326&permissions=8&scope=bot \n\t  Creator: 🔥NoChance🔥#2019\n\t```Information For Shinjan Bot``` \n\t**Help:**\n\t **Ban** .ban Omni \n\t **Kick** .kick omni\n\t **Uptime** .uptime \n\t**info** .info \n\t **Purge** .purge 200 \n\t **Mute** .mute Omni 1min Too Cool \n\t **Unmute** .unmute Omni Sry My Mistake, Your lit.")
+	return message.channel.send("**Shinjan's Bot Invite Link:**\n\t https://discordapp.com/api/oauth2/authorize?client_id=505877805058359326&permissions=8&scope=bot \n\t  Creator: 🔥NoChance🔥#2019\n\t```Information For Shinjan Bot``` \n\t**Help:**\n\t **Ban** .ban Omni \n\t **Kick** .kick omni\n\t **Uptime** .uptime \n\t**info** .info \n\t **Purge** .purge 200 \n\t **Mute** .mute Omni 1min Too Cool \n\t **Unmute** .unmute Omni Sry My Mistake, Your lit\n\t **Say** .say Omni Stop Being the best person in this universe!!")
 }
 
 module.exports.help = {
