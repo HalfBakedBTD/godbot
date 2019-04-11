df8af189666d888ce785403c73949c8065881659
OmniFZ <49297330+OmniFZ@users.noreply.github.com>
2019-04-11 17:01:18 -0400

    Update autorole.js

--- commands/autorole.js
+++ commands/autorole.js
@@ -1,6 +1,6 @@
 client.on('guildMemberAdd', member => {
 console.log('User' + member.user.tag + 'has joined the server!');
 
-var role = member.guild.roles.find('name', 'RandyPI | User');
+var role = member.guild.roles.find('name', 'ShinjanBot | User');
 member.addRole(role);
 })
