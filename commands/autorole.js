23240e77416d537962ebe645df999d4a5f853806
OmniFZ <49297330+OmniFZ@users.noreply.github.com>
2019-04-11 17:03:26 -0400

    Delete autorole.js

--- commands/autorole.js
+++
@@ -1,6 +0,0 @@
-client.on('guildMemberAdd', member => {
-console.log('User' + member.user.tag + 'has joined the server!');
-
-var role = member.guild.roles.find('name', 'ShinjanBot | User');
-member.addRole(role);
-})
