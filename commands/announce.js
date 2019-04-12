a9a26c861391e72a9c1fbcf87c2efcac421170a7
OmniFZ <49297330+OmniFZ@users.noreply.github.com>
2019-04-11 20:34:09 -0400

    Create announce.js

---
+++ commands/announce.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+
+exports.run = async (bot, message, args, ops) => {
+//this is ussles code
+	if (!message.member.roles.find(a => a.name === "@everyone")) { 
+		message.channel.send('Invalid permissions.');
+		return;
+	}
+    //look in the logs
+    // Check for input
+    if (!args[0]) return message.channel.send('Proper usage: !announce <message>');
+    
+    // Create Embed
+    const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setFooter('React to Vote.')
+        .setDescription(args.join(' '))
+        
+    let msg = await message.channel.send(embed)
+        .then(function (msg) {
+            message.delete({timeout: 1000});
+            }).catch(function(error) {
+            console.log(error);
+        });
+};
+
+exports.config = {
+    aliases: [ ]
+};
+
+module.exports.help = {
+  name: "announce
+}
