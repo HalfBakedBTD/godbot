9230d24310d93a8967aa8eecc2049b4b0a874622
OmniFZ <49297330+OmniFZ@users.noreply.github.com>
2019-04-13 16:19:37 -0400

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -7,8 +7,8 @@ module.exports.run = async (bot, message, args) => {
     let msgping2 = new Date() - msgping1;
     let pingembed = new Discord.RichEmbed()
         .setColor("BLUE")
-        .addField(API Ping: ', Math.floor(bot.ping) + ' ms ')
-        .addField(Math.floor(botping) + ' ms ')
+        .addField(API Ping: ', Math.floor(bot.ping) + " ms ")
+        .addField(Math.floor(botping) + " ms ")
         .addField(Your Ping: ', new Date().getTime() - message.createdTimestamp + " ms ")
         .setFooter(`Requested by ${message.author.tag}`);
     return message.channel.send(pingembed);
