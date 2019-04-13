54df9f943fd524e51871935509f1f86ed8ec6bd0
OmniFZ <49297330+OmniFZ@users.noreply.github.com>
2019-04-13 13:40:02 -0400

    Create dm.js

---
+++ commands/dm.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (bot, message, args) => {
+  
+   let dUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+   if (!dUser) return message.channel.send("Can't find user!");
+   if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("You can't use that command!")
+   let dMessage = args.join(" ").slice(22);
+   if(dMessage.length < 1) return message.reply('You must supply a message!')
+
+     dUser.send(`{member.guild.id: ${dMessage}`)
+  
+  
+
+  }
+  
+exports.config = {
+    aliases: [ ]
+};
+
+module.exports.help = {
+  name:"dm"
+}
