db7dfdee4586ac978154ba81379ddd22180c94d9
OmniFZ <49297330+OmniFZ@users.noreply.github.com>
2019-04-13 14:06:17 -0400

    Update dm.js

--- commands/dm.js
+++ commands/dm.js
@@ -4,11 +4,10 @@ module.exports.run = async (bot, message, args) => {
   
    let dUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
    if (!dUser) return message.channel.send("Can't find user!");
-   if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("You can't use that command!")
    let dMessage = args.join(" ").slice(22);
    if(dMessage.length < 1) return message.reply('You must supply a message!')
 
-     dUser.send(`<@${user.id}>: ${dMessage}`)
+     dUser.send(`from ${message.author.tag}: ${dMessage}`)
   
   
 
