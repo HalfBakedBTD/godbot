3b61e5e4f6fc2054113cf0f4310aae67acbc532a
OmniFZ <49297330+OmniFZ@users.noreply.github.com>
2019-04-13 14:16:21 -0400

    Update dm.js

--- commands/dm.js
+++ commands/dm.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
   
-   let dUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);.then(msg => msg.delete(2000));
+   let dUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
    if (!dUser) return message.channel.send("Can't find user!");
    let dMessage = args.join(" ").slice(22);
    if(dMessage.length < 1) return message.reply('You must supply a message!')
