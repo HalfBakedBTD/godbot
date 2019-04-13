a028c1895b49f2da9cb3250434aabdbfb889889a
OmniFZ <49297330+OmniFZ@users.noreply.github.com>
2019-04-13 13:27:35 -0400

    Update set.js

--- commands/set.js
+++ commands/set.js
@@ -1,30 +1,55 @@
+
 const Discord = require("discord.js");
 const db = require('quick.db');
 const prefixes = new db.table('PREFIXES');
+const levelson = new db.table("LEVELSON");
 const logchannels = new db.table('LOGCHANNELS');
 const events = new db.table("EVENTS");
-
+​
 module.exports.run = async (client,message) => {
-client.owners = [client.users.get("323940682857185321"),client.users.get("487707042224799757")];
+client.owners = [client.users.get("437578122100211713"),client.users.get("293148538886553602")];
   
 var prefix = await prefixes.fetch(`${message.guild.id}`);
 if(!prefix) {
   prefixes.set(`${message.guild.id}`, "!");
   prefix = '!'
 }
-
-let cmd = message.content.slice(prefix.length).split(' ').slice(1,2).join(' ');	
+​
+let cmd = message.content.slice(prefix.length).split(' ').slice(1,2).join(' '); 
 let args = message.content.slice(prefix.length).split(' ').slice(2).join(' ');
-
+​
 if(!message.member.hasPermission('ADMINISTRATOR') && message.author.id !== '420455135156764672' && message.author.id !== client.owners[0].id&&message.author.id !== client.owners[1].id)return message.channel.send('you need the ``"ADMINISTRATOR"`` permission to use this');
-if(!cmd || cmd === "info"){
+if(!cmd || cmd === "help"){
     let embed = new Discord.RichEmbed()
     .setTitle('How to use the Set command:')
     .setDescription('This is a moderation command used to set the server config')
-    .setColor(`RANDOM`)
+    .setColor(`BLUE`)
     .setTimestamp()
     .addField('**Command Usage:**', `\`\`\`css\n${prefix}set prefix {new prefix}\n${prefix}set levelSystem {on/off}\n${prefix}set logChannel {logChannel}\`\`\``)
      message.channel.send(embed);
+}
+if(cmd === "prefix"){
+  if(!args)return message.channel.send("I need a prefix!");  
+prefixes.fetch(`${message.guild.id}`).then(i => {
+    if(i == null||i == NaN||i == undefined){
+prefixes.set(`${message.guild.id}`, `${args}`);
+    } else {
+prefixes.set(`${message.guild.id}`, `${args}`);
+   }
+  });
+  message.channel.send(`New server prefix set to: ||${args}||`);
+}
+if(cmd === "levelSystem"){
+if(!args.toLowerCase().startsWith('on') && !args.toLowerCase().startsWith('off')) return message.channel.send("Off or On");  
+  if(args.toLowerCase().startsWith("on")){
+  levelson.set(`${message.guild.id}`, "true");
+  message.channel.send(`Succesfully \`Enabled\` the levelSystem for this sever`);
+  } else console.log(args)
+  if(args.toLowerCase().startsWith("off")){
+  levelson.set(`${message.guild.id}`, "false");
+  message.channel.send(`Succesfully \`Disabled\` the levelSystem for this sever`);
+  }
+}
 if(cmd === "logChannel"||cmd === "logs") {
   let channel = message.mentions.channels.first() || message.guild.channels.find(c => c.name.toLowerCase().includes(message.content.slice(prefix.length).slice(2)) && c.type == "text");
   if(!channel) return message.channel.send("oi ... i need the channel, right?");  
@@ -35,14 +60,14 @@ if(cmd === "logChannel"||cmd === "logs") {
   
   message.channel.send(`New logChannel set to: <#${channel.id}>`);
 }
-
+​
  }
-
-
+​
+​
 exports.config = {
     aliases: [ ]
 };
-
+​
 module.exports.help = {
   name: "set"
 } // owo uwu
