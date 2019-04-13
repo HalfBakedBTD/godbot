dc262ce9547d1c4d379dde70a3499dbd73d81ab0
OmniFZ <49297330+OmniFZ@users.noreply.github.com>
2019-04-13 16:23:47 -0400

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -3,15 +3,15 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     let msgping1 = new Date();
-    let botping = new Date() - message.createdAt;
-    let msgping2 = new Date() - msgping1;
-    let pingembed = new Discord.RichEmbed()
-        .setColor("BLUE")
-        .addField(API Ping: ', Math.floor(bot.ping) + " ms ")
-        .addField(Math.floor(botping) + " ms ")
-        .addField(Your Ping: ', new Date().getTime() - message.createdTimestamp + " ms ")
-        .setFooter(`Requested by ${message.author.tag}`);
-    return message.channel.send(pingembed);
+    let botping = new Date() - message.createdAt;
+    let msgping2 = new Date() - msgping1;
+    let pingembed = new Discord.RichEmbed()
+        .setColor("BLUE")
+        .addField('API Ping: ', Math.floor(bot.ping) + ' ms ')
+        .addField('Bot Ping: ', Math.floor(botping) + ' ms ')
+        .addField('Your Ping: ', new Date().getTime() - message.createdTimestamp + " ms ")
+        .setFooter(`Requested by ${message.author.tag}`);
+    return message.channel.send(pingembed);
 };
 
 exports.config = {
@@ -19,7 +19,5 @@ exports.config = {
 };
 
 module.exports.help = {
-    name: "ping"
+    name: "ping"
 };
-
-
