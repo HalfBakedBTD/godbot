cbb8a3610fd21fe2a7b7b0e765a9a648216bf643
OmniFZ <49297330+OmniFZ@users.noreply.github.com>
2019-04-13 14:31:41 -0400

    Update addrole.js

--- commands/addrole.js
+++ commands/addrole.js
@@ -1,38 +1,23 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
+  if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("You don't have the permission to do that!");
+  let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+  if(!rMember) return message.reply("Couldn't find that user!");
+  let role = args.join(" ").slice(22);
+  if(!role) return message.reply("Specify a role!");
+  let gRole = message.guild.roles.find(`name`, role);
+  if(!gRole) return message.reply("Couldn't find that role.");
 
-    if(!message.member.hasPermission("MANAGE_ROLES")) return message.channel.send("Sorry you cant do that try again when mod");
-    if(args[0] == "help"){
-        message.reply("Usage: !addrole <user> <role>");
-        return;
-    }
-    
-    if(!message.guild.member(bot.user).hasPermission("MANAGE_ROLES")){
-        return message.reply(":x: " + "| i need the \"MANAGE_ROLES\" permission!").catch(console.error);
-      }
+  if(rMember.roles.has(gRole.id)) return message.reply("This user already have that role.");
+  await(rMember.addRole(gRole.id));
 
-    let rMember = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(!rMember) return message.reply("You need to give me someone to add a role to, or the person you said me is incorrect");
-    let role = args.join(" ").slice(22);
-    if(!role) return message.reply("Please specify a role");
-    let gRole = message.guild.roles.find(a => a.name === role);
-    if(!gRole) return message.reply("You need to give me the name of the role you want me to add to that person, or the role you said is incorrect");
+    await message.channel.send(`***I just gave ${rMember} the ${gRole.name} role!***`)
 
-    if(rMember.roles.has(gRole.id)) return message.reply("They already have that role");
-    await(rMember.addRole(gRole.id));
-
-    try{
-       await rMember.send(`You have been given the role ${gRole.name}`)
-    }catch(e){
-        message.channel.send(`<@${rMember.id}> They have been given the role ${gRole.name} I tried to DM them but their DMs are locked`)
-    }
+    message.delete();
+  
 }
 
-exports.config = {
-    aliases: [  ]
-};
-
 module.exports.help = {
-    name: "arole"
+  name: "arole"
 }
