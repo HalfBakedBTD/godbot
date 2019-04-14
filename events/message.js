8f6eb1b6616b577d2f1cc89066b971609051e7d6
OmniFZ <49297330+OmniFZ@users.noreply.github.com>
2019-04-14 12:04:26 -0400

    Update message.js

--- events/message.js
+++ events/message.js
@@ -4,7 +4,10 @@ module.exports = async (client, message) => {
   if (message.author.bot) return;
   const config = require("../botconfig")
   const Discord = require ("discord.js")
-  const swearWords = ['shit', 'fuck', 'bitch', 'nigger', 'nigga', 'cunt', 'whore', 'fag', 'faggot', 'dick', 'cock', 'pussy', 'slut', 'bastard', 'fuck you', 'stfu'];
+  const db = require('quick.db');
+  const prefixes = new db.table('PREFIXES');
+  const logchannels = db.table("LOGCHANNEL");
+  const swearWords = ['shit', 'fuck', 'bitch', 'nigger', 'nigga', 'cunt', 'whore', 'fag', 'faggot', 'dick', 'cock', 'pussy', 'slut', 'bastard', 'fuck you'];
   if (config.swearfilter == true) {
     if (swearWords.some(word => message.content.toLowerCase().includes(word))) {
       message.delete().catch(O_o => {});
@@ -17,3 +20,45 @@ module.exports = async (client, message) => {
       return;
     }
   };
+
+  // Bot Mention Embed
+  if(message.content.toLowerCase() === '<@552705628741566475>'){
+    let embed = new Discord.RichEmbed()
+    .setTitle("ShinjanBot")
+    .addField("Prefix", `\`. \``, true)
+    .addField("Help", '...')
+    .setThumbnail(client.user.displayAvatarURL)
+    .setColor(`RANDOM`);
+    message.channel.send(embed);
+  };
+
+  
+  var prefix = await prefixes.fetch(`${message.guild.id}`);
+  if(!prefix) {
+    prefixes.set(`${message.guild.id}`, "!");
+    prefix = '!'
+  }
+  if (!message.content.toLowerCase().startsWith(prefix.toLowerCase())) return;
+
+  let ops = {
+    active: active
+  };
+  
+  this.config = require('../botconfig');
+    message.globalAdmin = !!this.config.owners.includes(message.author.id);
+  
+   // Our standard argument/command name definition.
+    // Our standard argument/command name definition.
+   let command = message.content.split(" ")[0].slice(prefix.length);
+  let args = message.content.split(" ").slice(1);
+  let cmd;
+  if (client.commands.has(command)) {
+    cmd = client.commands.get(command);
+  } else if (client.aliases.has(command)) {
+    cmd = client.commands.get(client.aliases.get(command));
+  }
+
+  if (cmd) {
+    cmd.run(client, message, args, ops);
+  }
+};
