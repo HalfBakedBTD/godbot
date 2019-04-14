26e1e148037697ec7dd64c743850a2b8aed2decd
OmniFZ <49297330+OmniFZ@users.noreply.github.com>
2019-04-13 22:10:22 -0400

    Create restart.js

---
+++ commands/restart.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (bot, message, args) => {
+    if(!message.author.id === "323940682857185321") return message.channel.send(":x: Only usable by Shinjan Bot Developers.");
+    message.channel.send('Restarting. Bye will be right back!').then(() => {
+    process.exit(2);
+   });
+};
+
+exports.config = {
+    aliases: [ ]
+};
+    
+module.exports.help = {
+        name: 'restart',
+        usage: 'restart',
+        description: 'Restarts the bot'
+};
