f6ecdb33ccb36f83e7b99602f33bb3caa7f33862
OmniFZ <49297330+OmniFZ@users.noreply.github.com>
2019-04-13 22:14:35 -0400

    Update index.js

--- index.js
+++ index.js
@@ -25,11 +25,11 @@ fs.readdir("./commands/", (err, files) => {
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
 
-  bot.user.setGame(`.info | ${client.members.size}`);
+  bot.user.setActivity(`.info | ${client.members.size}`);
 });
 
 bot.on('guildCreate', guild => {
-  bot.user.setGame(`.info | ${client.members.size}`);
+  bot.user.setActivity(`.info | ${client.members.size}`);
 });
 
 bot.on("message", async message => {
