e64b6cbda05539096efb1f4737efcc10a6ce6acb
OmniFZ <49297330+OmniFZ@users.noreply.github.com>
2019-04-13 20:25:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
 bot.commands = new Discord.Collection();
+require('./musicscript.js')
+
 
 fs.readdir("./commands/", (err, files) => {
 
