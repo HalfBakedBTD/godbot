fde0091525b69b2c3f12ff4c648e2eeb544ce7ee
OmniFZ <49297330+OmniFZ@users.noreply.github.com>
2019-04-14 17:06:13 -0400

    Create pin.js

---
+++ commands/pin.js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js");
+
+module.exports.run = (bot, message, args, discord) => {
+    let nid = args.join(' ');
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + "You do not have sufficient permissions to pin messages.");
+    if(!message.guild.me.hasPermission("MANAGE_MESSAGES")) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + " I do not have sufficient permissions to manage messages.");
+    if (args.length < 1) return message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + "Please supply a message ID.")
+      message.channel.fetchMessage(nid).then(id => {
+        id.pin().catch(err => {
+          message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + "Failed to pin" + id)
+          console.log(err)
+        })
+        message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " +  `Successfully pinned **${id}**.`)
+      }).catch(() => {
+        message.channel.send("**/" + message.guild + "/" + message.channel.name + "/** \n  " + `${nid} isn't a real message ID. Please enter a valid ID.`)
+      })
+  }
+
+exports.config = {
+enabled: true,
+guildOnly: false,
+aliases: [],
+};
+
+  module.exports.help = {
+    name: "pin"
+}
