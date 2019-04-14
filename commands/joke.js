41486d1d0ffc88a75e54c75ae1df6a197ea1fe15
OmniFZ <49297330+OmniFZ@users.noreply.github.com>
2019-04-14 11:44:27 -0400

    Update joke.js

--- commands/joke.js
+++ commands/joke.js
@@ -50,21 +50,7 @@ var jokes = [
     "What do you call someone with no body and no nose? Nobody knows.",
     "Where did the college-aged vampire like to shop? Forever 21.",
     "You heard of that new band 1023MB? They're good but they haven't got a gig yet.",
-    "Why did the crab never share? Because he's shellfish.",
-    "Today at the bank, an old lady asked me to help check her balance. So I pushed her over.",
-"I bought some shoes from a drug dealer. I don't know what he laced them with, but I've been tripping all day.",
-"I told my girlfriend she drew her eyebrows too high. She seemed surprised.",
-"My dog used to chase people on a bike a lot. It got so bad, finally I had to take his bike away.",
-"I'm so good at sleeping. I can do it with my eyes closed.",
-    "My boss told me to have a good day.. so I went home.",
-"Why is Peter Pan always flying? He neverlands.",
-"A woman walks into a library and asked if they had any books about paranoia. The librarian says "They're right behind you!",
-"The other day, my wife asked me to pass her lipstick but I accidentally passed her a glue stick. She still isn't talking to me.",
-"Why do blind people hate skydiving? It scares the hell out of their dogs.",
-"When you look really closely, all mirrors look like eyeballs.",
-"My friend says to me: What rhymes with orange I said: No it doesn't",
-"What do you call a guy with a rubber toe? Roberto.",
-"What did the pirate say when he turned 80 years old? Aye matey."
+    "Why did the crab never share? Because he's shellfish."
 ];
 
 module.exports.run = async (bot, message, args) => {
@@ -79,7 +65,7 @@ module.exports.run = async (bot, message, args) => {
 }
 
 exports.config = {
-    aliases: ['lol']
+    aliases: []
 };
 
 module.exports.help = {
